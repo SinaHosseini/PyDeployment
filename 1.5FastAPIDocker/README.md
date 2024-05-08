1241c133a2705531bc2ab283655c1c494222dfcf
SinaHosseini <sshosseinivaez@gmail.com>
2024-05-08 12:51:45 +0330

    update

---
+++ 1.5FastAPIDocker/README.md
@@ -0,0 +1,62 @@
+# Fast API | Docker
+
+I'll show you how to build a **Docker** image for **FastAPI** from scratch, based on the official Python image.
+
+## ToDo App
+
+In this api there is requests for `READ`, `ADD`, `UPDATE` and `REMOVE` from database.<br>
+Use the following commands to run the program.
+Database API:
+
+```
+uvicorn main:app --reload
+```
+
+## Build Docker
+
+The most common way to do it is to have a file requirements.txt with the package names and their versions, one per line.
+
+```
+pip install -r requirements.txt
+```
+
+<strong>Create the FastAPI Code</strong><br><hr>
+
+- Create an `app` directory and enter it.<br>
+- Create a [`main.py`](https://github.com/MohamadNematizadeh/PyDeploy/blob/8154e5cf30b6db787783de3050bfd7dd893aa002/FastAPI/1.5.FastaAPI_Docke/app/main.py) file.
+
+<strong>DockerFile</strong><br><hr>
+Now in the same project directory create a file [`Dockerfile`](https://github.com/SinaHosseini/PyDeployment/blob/4e50ed613a7b8b36830d7a54f275f1ab76a26be2/1.5FastAPIDocker/Dockerfile)
+
+<strong>Build the Docker Image</strong><br><hr>
+Now that all the files are in place, let's build the container image.
+
+- Go to the project directory (in where your `Dockerfile` is, containing your `app` directory).
+- Build your FastAPI image:
+
+```
+docker build -t myimage .
+```
+
+<strong>Start the Docker Container</strong><br><hr>
+
+- Run a container based on your image:
+
+```
+docker run -d --name mycontainer -p 80:80 myimage
+```
+
+<strong>Interactive API docs</strong><br><hr>
+Now you can go to http://192.168.99.100/docs or http://127.0.0.1/docs (or equivalent, using your Docker host).
+
+You will see the automatic interactive API documentation (provided by Swagger UI):<br>
+![img](images/img1.jpg)
+<strong>Alternative API docs</strong><br><hr>
+And you can also go to http://192.168.99.100/redoc or http://127.0.0.1/redoc (or equivalent, using your Docker host).
+
+You will see the alternative automatic documentation (provided by ReDoc):
+![img2](images/img2.png)
+
+## Docker command
+Docker commands can be followed from the link below:<br>
+[Cheat Sheet](https://docs.docker.com/get-started/docker_cheatsheet.pdf)
--- 1.5FastAPIDocker/app/main.py
+++ 1.5FastAPIDocker/app/main.py
@@ -1,63 +1,67 @@
-import io
-import cv2
-from skimage import io as io1
-from fastapi import FastAPI ,HTTPException, status
-from fastapi.responses import StreamingResponse
+import sqlite3
+from fastapi import FastAPI, Form, HTTPException
 
 app = FastAPI()
 
+
+def database():
+    con = sqlite3.connect("tasks.db")
+    cur = con.cursor()
+
+    return con, cur
+
+
 @app.get("/")
-def introducing():
-    return "This API show pawn, bishop, knight, rook, queen, and king information ♟️"
+def read_root():
+    return "Hi, welcome to my api In this api there is  requests for read, add, update and remove database--created by Sina Hosseini"
 
-@app.get("/pieces")
-def pieces():
-    return "pawn ♙, ♟  bishop ♗, ♝  knight ♘, ♞  rook  ♖, ♜ queen ♕, ♛  king ♔, ♚"
 
-@app.get("/pieces/{pieces_name}")
-def pieces_name(pieces_name: str):
-    if pieces_name == "pawn":
-        return "The pawn (♙, ♟) is the most numerous and weakest piece in the game of chess. It may move one square directly forward, it may move two squares directly forward on its first move, and it may capture one square diagonally forward."
-    elif pieces_name == "bishop":
-        return "The bishop (♗, ♝) is a piece in the game of chess. It moves and captures along diagonals without jumping over intervening pieces. Each player begins the game with two bishops. The starting squares are c1 and f1 for White's bishops, and c8 and f8 for Black's bishops."
-    elif pieces_name == "knight":
-        return "The knight (♘, ♞) is a piece in the game of chess, represented by a horse's head and neck. It moves two squares vertically and one square horizontally, or two squares horizontally and one square vertically, jumping over other pieces. Each player starts the game with two knights on the b- and g-files, each located between a rook and a bishop."
-    elif pieces_name == "rook":
-        return "The rook (♖, ♜) is a piece in the game of chess. It may move any number of squares horizontally or vertically without jumping, and it may capture an enemy piece on its path; additionally, it may participate in castling. Each player starts the game with two rooks, one in each corner on their own side of the board."
-    elif pieces_name == "queen":
-        return "The queen (♕, ♛) is the most powerful piece in the game of chess. It can move any number of squares vertically, horizontally or diagonally, combining the powers of the rook and bishop. Each player starts the game with one queen, placed in the middle of the first rank next to the king. Because the queen is the strongest piece, a pawn is promoted to a queen in the vast majority of cases."
-    elif pieces_name == "king":
-        return "The king (♔, ♚) is the most important piece in the game of chess. It may move to any adjoining square; it may also perform, in tandem with the rook, a special move called castling. If a player's king is threatened with capture, it is said to be in check, and the player must remove the threat of capture immediately. If this cannot be done, the king is said to be in checkmate, resulting in a loss for that player. A player cannot make any move that places their own king in check. Despite this, the king can become a strong offensive piece in the endgame or, rarely, the middlegame."
+@app.get("/items")
+def read_data():
+    _, cur = database()
+    res = cur.execute("SELECT * FROM tasks")
+    res = res.fetchall()
+
+    return res
+
+
+@app.post("/items")
+def add_data(id: int = Form(), tittle: str = Form(), description: str = Form(), time: str = Form(), status: int = Form(0)):
+    con, cur = database()
+    cur.execute(
+        f"INSERT INTO tasks (id, tittle, description, time, status) VALUES (?, ?, ?, ?, ?)",
+        (id, tittle, description, time, status)
+    )
+    con.commit()
+
+    return read_data()
+
+
+@app.put("/items/{id}")
+def update_data(id: int, tittle: str = Form(), description: str = Form(), time: str = Form(), status: int = Form()):
+    con, cur = database()
+    cur.execute("SELECT id FROM tasks")
+    exs_id = [row[0] for row in cur.fetchall()]
+    if id not in exs_id:
+        raise HTTPException(status_code=404, detail="Item not found")
+
     else:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="please enter correct pieces chess")
-    
+        cur.execute("UPDATE tasks SET tittle=?, description=?, time=?, status=? WHERE id=?",
+                    (tittle, description, time, status, id))
+        con.commit()
 
-@app.get("/pieces/{pieces_name}/image")
-def images(pieces_name):
-    if pieces_name == "pawn":
-        image = io1.imread("pics/pawn.png")
-        _, encoded_image = cv2.imencode(".png", image)
-        return StreamingResponse(io.BytesIO(encoded_image.tobytes()), media_type="image/png")
-    elif pieces_name == "bishop":
-        image = io1.imread("pics/bishop.png")
-        _, encoded_image = cv2.imencode(".png", image)
-        return StreamingResponse(io.BytesIO(encoded_image.tobytes()), media_type="image/png")
-    elif pieces_name == "knight":
-        image = io1.imread("pics/knight.png")
-        _, encoded_image = cv2.imencode(".png", image)
-        return StreamingResponse(io.BytesIO(encoded_image.tobytes()), media_type="image/png")
-    elif pieces_name == "rook":
-        image = io1.imread("pics/rook.png")
-        _, encoded_image = cv2.imencode(".png", image)
-        return StreamingResponse(io.BytesIO(encoded_image.tobytes()), media_type="image/png")
-    elif pieces_name == "queen":
-        image = io1.imread("pics/queen.png")
-        _, encoded_image = cv2.imencode(".png", image)
-        return StreamingResponse(io.BytesIO(encoded_image.tobytes()), media_type="image/png")
-    elif pieces_name == "king":
-        image = io1.imread("pics/king.png")
-        _, encoded_image = cv2.imencode(".png", image)
-        return StreamingResponse(io.BytesIO(encoded_image.tobytes()), media_type="image/png")
+    return read_data()
+
+
+@app.delete("/items/{id}")
+def delete_item(id: int):
+    con, cur = database()
+    cur.execute("SELECT id FROM tasks")
+    exs_id = [row[0] for row in cur.fetchall()]
+    if id not in exs_id:
+        raise HTTPException(status_code=404, detail="Item not found")
+
     else:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="please enter correct pieces chess")
-    
+        cur.execute("DELETE FROM tasks WHERE id=?", (id,))
+        con.commit()
+        return {"message": "Item deleted"}
