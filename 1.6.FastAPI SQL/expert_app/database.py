39754fbf18c9871269577fe15cb0a60a23dd6915
SinaHosseini <sshosseinivaez@gmail.com>
2024-05-20 17:47:03 +0330

    update

---
+++ 1.6.FastAPI SQL/expert_app/database.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://sina:8281@localhost:5432/database-sina"
+# SQLALCHEMY_DATABASE_URL = "postgresql://root:usvwZFdye72zqtOd0anFj7N2@sina-postgre:5432/postgres"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
