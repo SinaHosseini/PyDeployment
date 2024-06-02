af060f8eb65f11ac6b3d08b6169c19db3db6c08a
SinaHosseini <sshosseinivaez@gmail.com>
2024-06-02 15:48:46 +0330

    update

--- Streamlit/2.2.Streamlit-SQLModel/models.py
+++ Streamlit/2.2.Streamlit-SQLModel/models.py
@@ -1,23 +1,29 @@
-from typing import Optional
-from sqlmodel import Field, Session, SQLModel, create_engine, select, Relationship
+from typing import Optional, List
+from sqlmodel import Field, SQLModel, Relationship
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=['bcrypt'], deprecated="auto")
 
 
 class User(SQLModel, table=True):
-    __tablename__ = "users"
-    __table_args__ = {'extend_existing': True}
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str
     email: str
-    password: Optional[str]
+    hashed_password: str
 
-    messages: list['Message'] = Relationship(back_populates='user')
+    messages: List['Message'] = Relationship(back_populates='user')
+
+    def set_password(self, password: str):
+        self.hashed_password = pwd_context.hash(password)
+
+    def verify_password(self, password: str) -> bool:
+        return pwd_context.verify(password, self.hashed_password)
+
 
 class Message(SQLModel, table=True):
-    __tablename__ = "messages"
-    __table_args__ = {'extend_existing': True}
     id: Optional[int] = Field(default=None, primary_key=True)
     text: str
     type: str
     user_id: int = Field(default=None, foreign_key='user.id')
-    
+
     user: User = Relationship(back_populates='messages')
