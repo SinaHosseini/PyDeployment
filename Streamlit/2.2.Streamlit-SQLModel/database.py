79640d43df72fff48c39542d1d2a5fb4b7e7c28d
SinaHosseini <sshosseinivaez@gmail.com>
2024-06-04 11:59:13 +0330

    update

--- Streamlit/2.2.Streamlit-SQLModel/database.py
+++ Streamlit/2.2.Streamlit-SQLModel/database.py
@@ -2,9 +2,9 @@ from sqlmodel import SQLModel, Session, create_engine, select
 from models import User, Message, Optional
 
 
-# DATABASE_URL = 'sqlite:///database.db'
+DATABASE_URL = 'sqlite:///database.db'
 # DATABASE_URL = "postgresql://sina:ramze_sina@localhost:5432/database_sina"
-DATABASE_URL = "postgresql://root:z8aCVH29rqcz4lQ85n2a7JHF@sina-postgre:5432/postgres"
+# DATABASE_URL = "postgresql://root:z8aCVH29rqcz4lQ85n2a7JHF@sina-postgre:5432/postgres"
 engine = create_engine(DATABASE_URL)
 
 
